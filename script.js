598992248961a5e6c114cd71bb8f48dfe009ebff
Jawad Hossain <jawadhossain3015@gmail.com>
2026-05-06 20:52:00 -0400

    Removed fading affects of nav-links, added smoother transitions upon button clicks
    Removed scroll event listener for header opacity and added smooth scrolling for contact button.

--- script.js
+++ script.js
@@ -2,16 +2,13 @@ const menuIcon = document.querySelector('#menu-icon');
 const navLinks = document.querySelector('.nav-links');
 
 menuIcon.onclick = () => {
-    navLinks.classList.toggle('active')
-}
+    navLinks.classList.toggle('active');
+};
 
-const header = document.querySelector('header');
+document.getElementById('view-projects-btn').onclick = () => {
+    document.getElementById('projects').scrollIntoView({ behavior: 'smooth' });
+};
 
-window.addEventListener('scroll', () => {
-    const fadeStart = 50;
-    const fadeEnd = 200;
-    const scrollY = window.scrollY;
-    const opacity = 1 - Math.min(Math.max((scrollY - fadeStart) / (fadeEnd - fadeStart), 0), 1);
-    header.style.opacity = opacity;
-    header.style.pointerEvents = opacity === 0 ? 'none' : 'auto';
-});
+document.getElementById('contact-btn').onclick = () => {
+    document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
+};
